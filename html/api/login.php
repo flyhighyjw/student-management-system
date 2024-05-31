eded1b9842b987ab7897da906d7251f52b2786cf
ehfhfhd <191003cyrano@gmail.com>
2024-05-31 15:45:12 +0000

    update login

--- html/api/login.php
+++ html/api/login.php
@@ -3,19 +3,23 @@ include '../dbconn.php';
 header('Content-Type: application/json');
 
 $id = $_POST["username"];
-$pw = $_POST["password"];
+$pw = md5($_POST["password"]); // MD5 암호화
 
 $sql = "SELECT * FROM USER WHERE user_id = '$id' AND user_pw = '$pw'";
 $result = $conn->query($sql);
 $row = mysqli_fetch_array($result);
 
+$response = array();
+
 if ($result->num_rows > 0) {
     setcookie('userid', $id, time() + 86400, "/");
-    header("location: ../index.html");
+    $response['success'] = true;
+    $response['message'] = '로그인 성공';
 } else {
-    echo "<script>
-            alert('아이디 또는 비밀번호가 잘못되었습니다.');
-            window.location.href = '../login.html';
-          </script>";
+    $response['success'] = false;
+    $response['message'] = '아이디 또는 비밀번호가 잘못되었습니다.';
 }
+
+echo json_encode($response);
+$conn->close();
 ?>
--- html/api/register.php
+++ html/api/register.php
@@ -6,7 +6,7 @@ $data = json_decode(file_get_contents('php://input'), true);
 
 // 사용자가 입력한 값
 $userid = $data['userid'];
-$password = $data['password'];
+$password = md5($data['password']);
 $username = $data['username'];
 $phone = $data['phone'];
 $track_no = $data['track']; // 트랙 번호를 직접 받음
